f8d99fef6a1b6be0c2ba03907e4fd988b3f997dc
Chris Dombroski <cdombroski@icanttype.org>
2014-02-02 10:14:32 -0500

    Initial import for euler002

---
+++ euler-utils/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to euler-utils
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ euler-utils/test/euler_utils/core_test.clj
@@ -0,0 +1,7 @@
+(ns euler-utils.core-test
+  (:use clojure.test
+        euler-utils.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
