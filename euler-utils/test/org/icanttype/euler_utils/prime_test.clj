e485eaafcf8eade4b829a88f318132524cbec3a5
cdombroski <cdombroski@icanttype.org>
2014-03-06 16:31:13 -0500

    Prime utilities

---
+++ euler-utils/test/org/icanttype/euler_utils/prime_test.clj
@@ -0,0 +1,10 @@
+(ns org.icanttype.euler_utils.prime_test
+  (:use midje.sweet
+        org.icanttype.euler_utils.prime))
+
+(facts "prime sequence"
+       (fact "First 5 primes are 2 3 5 7 11"
+             (take 5 (sieve)) => [2 3 5 7 11])
+       (fact "prime factors"
+             (prime-factor 4) => {2 2}
+             (prime-factor 15) => {3 1, 5 1}))
