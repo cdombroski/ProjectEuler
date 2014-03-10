23b33986a9726c1e02293413c8aba6d35ab8bf12
Chris Dombroski <cdombroski@icanttype.org>
2014-03-10 14:58:18 -0400

    Working gcd

--- euler-utils/src/org/icanttype/euler_utils/factor.clj
+++ euler-utils/src/org/icanttype/euler_utils/factor.clj
@@ -3,12 +3,26 @@
 
 (defn gcd
   ([x] (math/abs x))
-  ([x y] x)
+  ([x y]
+   (cond
+     (zero? x) (math/abs y)
+     (zero? y) (math/abs x)
+     :else (loop [a x
+                  b y
+                  d 1]
+             (cond
+               (and (even? a) (even? b)) (recur (/ a 2) (/ b 2) (*' d 2))
+               (even? a) (recur (/ a 2) b d)
+               (even? b) (recur a (/ b 2) d)
+               :else (if (>= a b)
+                       (*' d (gcd b (/ (- a b) 2)))
+                       (*' d (gcd a (/ (- b a) 2))))))))
   ([x y & more] (reduce gcd x (cons y more))))
 
 (defn lcm
   ([x] (math/abs x))
-  ([x y] (if (= 0 x y)
-           0
-           (*' (/ (math/abs x) (gcd x y)) (math/abs y))))
+  ([x y]
+   (if (= 0 x y)
+     0
+     (*' (/ (math/abs x) (gcd x y)) (math/abs y))))
   ([x y & more] (apply *' (/ (math/abs x) (apply gcd x y more)) (map math/abs (cons y more)))))
