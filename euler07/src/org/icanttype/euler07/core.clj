ced13742164d42b18c7631baff2b3c185b17b0d2
Chris Dombroski <cdombroski@icanttype.org>
2014-03-11 15:02:26 -0400

    A working solution... but too slow

--- euler07/src/org/icanttype/euler07/core.clj
+++ euler07/src/org/icanttype/euler07/core.clj
@@ -1,6 +1,6 @@
-(ns org.icanttype.euler07.core)
+(ns org.icanttype.euler07.core
+  (:require [org.icanttype.euler-utils.prime :refer [sieve]])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main [& args]
+  (println (nth (sieve) 10000)))
