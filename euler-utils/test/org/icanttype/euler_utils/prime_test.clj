29aa88930d9562afc31f8b7f3384981b736ff23b
Chris Dombroski <cdombroski@icanttype.org>
2014-03-12 11:25:06 -0400

    Fix euler-utils test.

--- euler-utils/test/org/icanttype/euler_utils/prime_test.clj
+++ euler-utils/test/org/icanttype/euler_utils/prime_test.clj
@@ -6,7 +6,7 @@
        (fact "Primes <= 20"
              (sieve 20) => [2 3 5 7 11 13 17 19])
        (fact "First 5 primes"
-             (take 5 (primes)) => [2 3 5 7 11])
+             (take 5 primes) => [2 3 5 7 11])
        (fact "prime factors"
              (prime-factor 4) => {2 2}
              (prime-factor 15) => {3 1, 5 1}))
